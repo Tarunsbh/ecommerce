e488390703ba6077b347fd711f8514f3cb4e9a73
Tarun Sabharwal <tarunsbh@gmail.com>
2023-11-02 01:43:35 +0530

    routes

--- src/Routers/CustomerRouters.jsx
+++ src/Routers/CustomerRouters.jsx
@@ -5,6 +5,10 @@ import Navigation from "../customer/components/navigation/Navigation";
 import Footer from "../customer/components/footer/Footer";
 import Cart from "../customer/components/Cart/Cart";
 import Product from "../customer/components/product/Product";
+import ProductDetails from "../customer/components/productDetails/ProductDetails";
+import Checkout from "../customer/components/Checkout/Checkout";
+import Order from "../customer/components/Order/Order";
+import OrderDetails from "../customer/components/Order/OrderDetails";
 
 const CustomerRouters = () => {
   return (
@@ -15,8 +19,11 @@ const CustomerRouters = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/:lavelOne/:lavelTwo/:lavelThree" element={<Product />} />
         <Route path="/product" element={<Product />} />
+        <Route path="/product/:productId" element={<ProductDetails />} />
+        <Route path="/account/order" element={<Order />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/acount/order/:orderId" element={<OrderDetails />} />
       </Routes>
       <div>
         <Footer />
--- src/customer/components/data/navigationData.js
+++ src/customer/components/data/navigationData.js
@@ -1,17 +1,73 @@
 export const navigation = {
+  categories: [
+    {
+      id: "Shop",
+      name: "Shop",
+      featured: [
+        {
+          name: "New Arrivals",
+          href: "#",
+          imageSrc:
+            "https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg",
+          imageAlt:
+            "Models sitting back to back, wearing Basic Tee in black and bone.",
+        },
+        {
+          name: "Basic Tees",
+          href: "#",
+          imageSrc:
+            "https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg",
+          imageAlt:
+            "Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.",
+        },
+      ],
+      sections: [
+        {
+          id: "allcategories",
+          name: "All Categories",
+          items: [
+            { name: "Wellness", href: "#" },
+            { name: "Skin Care", href: "/product" },
+            { name: "Color", href: "#" },
+            { name: "Personal Care", href: "#" },
+            { name: "Home Care", href: "#" },
+            { name: "Laundary Care", href: "#" },
+            { name: "Food & Beverages", href: "#" },
+            { name: "Auto Care", href: "#" },
+            { name: "Lifestyle", href: "#" },
+            { name: "Auto Care", href: "#" },
+            { name: "Watches & Jewellery", href: "#" },
+            { name: "Others", href: "#" },
+          ],
+        },
+        // {
+        //   id: "accessories",
+        //   name: "Accessories",
+        //   items: [
+        //     { name: "Watches", href: "#" },
+        //     { name: "Wallets", href: "#" },
+        //     { name: "Bags", href: "#" },
+        //     { name: "Sunglasses", href: "#" },
+        //     { name: "Hats", href: "#" },
+        //     { name: "Belts", href: "#" },
+        //   ],
+        // },
+        // {
+        //   id: "brands",
+        //   name: "Brands",
+        //   items: [
+        //     { name: "Full Nelson", href: "#" },
+        //     { name: "My Way", href: "#" },
+        //     { name: "Re-Arranged", href: "#" },
+        //     { name: "Counterfeit", href: "#" },
+        //     { name: "Significant Other", href: "#" },
+        //   ],
+        // },
+      ],
+    },
+  ],
   pages: [
-    { name: "Home", href: "/" },
-    { name: "Wellness", href: "#" },
-    { name: "Skin Care", href: "/product" },
-    { name: "Color", href: "#" },
-    { name: "Personal Care", href: "#" },
-    { name: "Home Care", href: "#" },
-    { name: "Laundary Care", href: "#" },
-    { name: "Food & Beverages", href: "#" },
-    { name: "Auto Care", href: "#" },
-    { name: "Lifestyle", href: "#" },
-    { name: "Auto Care", href: "#" },
-    { name: "Watches & Jewellery", href: "#" },
-    { name: "Others", href: "#" },
+    { name: "About Us", href: "#" },
+    { name: "Contact Us", href: "#" },
   ],
 };
